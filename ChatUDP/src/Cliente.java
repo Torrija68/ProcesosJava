a5f378bedb0a4e5fdc1e2075a63e8c2d23f8b106
Torrija68 <estudiantes1605@gmail.com>
2024-02-23 20:18:13 +0100

    ChatUDP

--- ChatUDP/src/Cliente.java
+++ ChatUDP/src/Cliente.java
@@ -1,36 +1,64 @@
-import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 import java.io.IOException;
 import java.net.*;
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.swing.*;
 
 public class Cliente extends JFrame implements ActionListener {
-    int puerto = 8888;
-    String direccion = "225.0.0.1";
-    String nombre;
-    MulticastSocket msk;
-    InetAddress grupo;
-    DatagramPacket envio;
-    DatagramPacket receptor;
-    DatagramSocket ds;
-    byte[] bufferEnvio = new byte[256];
-    byte[] bufferReceptor = new byte[256];
+    public int puerto;
+    public InetAddress servidor;
+    private DatagramSocket ds;
+    private String nombre;
+    private Set<String> nombresRegistrados;
+    private Set<String> mensajesRecibidos = new HashSet<>();
+    private byte[] bufferReceptor = new byte[256];
+    private byte[] bufferEnvio = new byte[256];
     private JTextArea txtChat;
     private JTextField txtMensaje;
     private Button btnEnviar;
     private JScrollPane scroll;
 
     public Cliente() {
-
-        nombre = JOptionPane.showInputDialog("Ingresa tu nombre");
-        if(nombre != null){
-            initComponents();
-            recibirMensajes();
+        nombresRegistrados = new HashSet<>();
+        nombre = obtenerNombreUsuario();
+        initComponents();
+        try {
+            ds = new DatagramSocket();
+            servidor = InetAddress.getByName("localhost");
+            puerto = 5555;
+            registrarUsuario(nombre);
+            Thread HiloMensajes = new Thread(this::recibirMensajes);
+            HiloMensajes.start();
+        } catch (SocketException e) {
+            throw new RuntimeException(e);
+        } catch (UnknownHostException e) {
+            throw new RuntimeException(e);
         }
+        addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                super.windowClosing(e);
+                cerrarSocket();
+            }
+        });
 
     }
-    private void initComponents(){
+
+    private void cerrarSocket() {
+        if(ds != null && !ds.isClosed()){
+            ds.close();
+            txtChat.append(nombre + " desconectado");
+        }
+    }
+
+    private void initComponents() {
+
         txtChat = new JTextArea();
         txtChat.setEditable(false);
         txtChat.setBorder(BorderFactory.createTitledBorder("Chat"));
@@ -49,56 +77,89 @@ public class Cliente extends JFrame implements ActionListener {
         add(scroll, BorderLayout.CENTER);
         add(panelEnviarMensaje, BorderLayout.SOUTH);
 
-        btnEnviar.addActionListener( this);
+        btnEnviar.addActionListener(this);
 
         setVisible(true);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
-        setSize(400,500);
+        setSize(400, 500);
         setLocationRelativeTo(null);
-
+    }
+    private void registrarUsuario(String nombreUsuario) {
+        String mensaje = nombreUsuario + ":REGISTRO";
+        enviarMensaje(mensaje);
     }
 
-    public void recibirMensajes(){
-            try {
-                ds = new DatagramSocket();
-                grupo = InetAddress.getByName(direccion);
-
-                msk = new MulticastSocket(puerto);
-                msk.joinGroup(grupo);
-
-                while (true){
-                    receptor = new DatagramPacket(bufferReceptor,bufferReceptor.length,grupo,puerto);
-                    msk.receive(receptor);
-                    String mensaje = new String(receptor.getData(),0, receptor.getLength());
-                    txtChat.append(mensaje + "\n");
-                }
-            } catch (SocketException e) {
-                throw new RuntimeException(e);
-            } catch (UnknownHostException e) {
-                throw new RuntimeException(e);
-            } catch (IOException e) {
-                throw new RuntimeException(e);
+    private String obtenerNombreUsuario() {
+        String nombreUsuario = null;
+        while (nombreUsuario == null || nombreUsuario.trim().isEmpty()) {
+            nombreUsuario = JOptionPane.showInputDialog("Ingrese su nombre de usuario");
+            if (nombreUsuario == null || nombreUsuario.trim().isEmpty()) {
+                JOptionPane.showMessageDialog(this, "El nombre de usuario no puede estar vacío");
             }
+        }
+        return nombreUsuario;
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        String mensaje = txtMensaje.getText();
-        if(!mensaje.isEmpty()){
-            enviarMensaje(nombre +": "+ mensaje);
+        if (e.getSource() == btnEnviar) {
+            String mensaje = nombre + ":" + txtMensaje.getText();
+            enviarMensaje(mensaje);
             txtMensaje.setText("");
         }
     }
-    private void enviarMensaje(String mensaje){
+
+    public void recibirMensajes() {
+        while (true) {
+            DatagramPacket receptor = new DatagramPacket(bufferReceptor, bufferReceptor.length);
+            try {
+                ds.receive(receptor);
+                String mensaje = new String(receptor.getData(), 0, receptor.getLength());
+                if (mensaje.equals("REGISTRO_EXITOSO")) {
+                    JOptionPane.showMessageDialog(this, "Registro exitoso!");
+                } else if (mensaje.equals("NOMBRE_EN_USO")) {
+                    JOptionPane.showMessageDialog(this, "El nombre de usuario ya está en uso. Por favor, elija otro.");
+                    nombre = obtenerNombreUsuario();
+                    registrarUsuario(nombre);
+                } else {
+
+                    agregarMensaje(mensaje);
+                }
+            } catch (IOException e) {
+                if(ds != null){
+                    ds.close();
+                }
+
+            }
+        }
+    }
+    private void enviarMensaje(String mensaje) {
         try {
             bufferEnvio = mensaje.getBytes();
-            envio = new DatagramPacket(bufferEnvio,bufferEnvio.length,grupo,puerto);
-            msk.send(envio);
+            DatagramPacket envio = new DatagramPacket(bufferEnvio, bufferEnvio.length, servidor, puerto);
+            ds.send(envio);
+            agregarMensaje(mensaje);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
     }
+    private void agregarMensaje(String mensaje) {
+        String[] partesMensaje = mensaje.split(":", 2);
+        if (!(partesMensaje.length == 2 && partesMensaje[1].equals("REGISTRO"))) {
+            String messageContent = partesMensaje[1].trim(); // Extract message content
+            if (!mensajesRecibidos.contains(messageContent)) { // Check if message is unique
+                SwingUtilities.invokeLater(() -> {
+                    if (!mensaje.trim().isEmpty()) {
+                        txtChat.append(mensaje + "\n");
+                        mensajesRecibidos.add(messageContent); // Add message to set
+                    }
+                });
+            }
+        }
+    }
+
     public static void main(String[] args) {
         new Cliente();
     }
+
 }
--- ChatUDP/src/MainServer.java
+++ ChatUDP/src/MainServer.java
@@ -1,54 +1,94 @@
-import javax.xml.crypto.Data;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
 import java.io.IOException;
-import java.net.*;
-import java.util.List;
-import java.util.Scanner;
-import java.util.concurrent.CopyOnWriteArrayList;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketException;
 
 public class MainServer {
-    int puerto = 8888;
-    String direccion = "230.0.0.0";
-    private MulticastSocket msk;
-    private InetAddress grupo;
-    DatagramPacket envio;
-    DatagramPacket receptor;
-    byte[] bufferEnvio = new byte[256];
-    byte[] bufferReceptor = new byte[256];
-    List<Integer> clientes;
-
-    public MainServer(){
-        try {
-            msk = new MulticastSocket();
-            grupo = InetAddress.getByName(direccion);
-            System.out.println("Esperando cliente");
-
-            Thread HiloReceptor = new Thread(() -> {
-                    try {
-                        while (true) {
-                            receptor = new DatagramPacket(bufferReceptor, bufferReceptor.length);
-                            msk.receive(receptor);
-
-                        }
-                    } catch (IOException e) {
-                        throw new RuntimeException(e);
-                    }
-
-                });
-            HiloReceptor.start();
-
-
-            } catch (UnknownHostException ex) {
-            throw new RuntimeException(ex);
-        } catch (IOException ex) {
-            throw new RuntimeException(ex);
-        }
-
-
-    }
+    private static DatagramSocket ds;
+    private byte[] bufferEnvio = new byte[256];
+    private byte[] bufferReceptor = new byte[256];
+    private static Map<String, InetAddress> clientes = new HashMap<>();
+    private static Set<String> nombresRegistrados = new HashSet<>();
 
     public static void main(String[] args) {
-        new MainServer();
+        iniciarServidor(5555);
+    }
+
+    private static void iniciarServidor(int puerto) {
+        try {
+            ds = new DatagramSocket(puerto);
+            System.out.println("Servidor Iniciado en " + puerto);
+            while (true) {
+                byte[] bufferReceptor = new byte[256];
+                DatagramPacket receptor = new DatagramPacket(bufferReceptor, bufferReceptor.length);
+                ds.receive(receptor);
+                InetAddress direccionCliente = receptor.getAddress();
+                int puertoCliente = receptor.getPort();
+                String idCliente = direccionCliente.getHostAddress() + ":" + puertoCliente;
+                clientes.put(idCliente, direccionCliente);
+
+                String mensajeRecibido = new String(receptor.getData(), 0, receptor.getLength());
+
+                if(!mensajeRecibido.isEmpty()) {
+                    String[] partesMensaje = mensajeRecibido.split(":", 2);
+                    if (partesMensaje.length == 2 && partesMensaje[1].equals("REGISTRO")) {
+                        String nombreUsuario = partesMensaje[0];
+                        if (!nombresRegistrados.contains(nombreUsuario)) {
+                            nombresRegistrados.add(nombreUsuario);
+                            enviarMensaje("REGISTRO_EXITOSO", direccionCliente, puertoCliente);
+                        } else {
+                            enviarMensaje("NOMBRE_EN_USO", direccionCliente, puertoCliente);
+                        }
+                    } else if (partesMensaje.length == 2) {
+                        String nombreUsuario = partesMensaje[0];
+                        String contenidoMensaje = partesMensaje[1];
+                        enviarMensajeATodos(nombreUsuario + ": " + contenidoMensaje, direccionCliente, puertoCliente);
+                    }
+                }
+            }
+        } catch (SocketException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } finally {
+            if (ds != null) {
+                ds.close();
+            }
+        }
+    }
+
+    private static void manejarDesconexion(String idCliente) {
+        clientes.remove(idCliente);
+    }
+
+    private static void enviarMensajeATodos(String mensaje, InetAddress direccionOrigen, int puertoOrigen) {
+        for (Map.Entry<String, InetAddress> entry : clientes.entrySet()) {
+            String idCliente = entry.getKey();
+            InetAddress direccionCliente = entry.getValue();
+            int puertoCliente = Integer.parseInt(idCliente.substring(idCliente.lastIndexOf(":") + 1));
+            if (!idCliente.equals(direccionOrigen.getHostAddress() + ":" + puertoOrigen)) {
+                enviarMensaje(mensaje, direccionCliente, puertoCliente);
+            }
+        }
+    }
+
+    private static void enviarMensaje(String mensaje, InetAddress direccionCliente, int puertoCliente) {
+        try {
+            byte[] bufferEnvio = mensaje.getBytes();
+            DatagramPacket envio = new DatagramPacket(bufferEnvio, bufferEnvio.length, direccionCliente,
+                    puertoCliente);
+            ds.send(envio);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
+
+
 
 
